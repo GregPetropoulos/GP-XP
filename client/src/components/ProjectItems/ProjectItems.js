ea9042acad27616f2c96c6f20a8a0081e015f940
GP <gregpetropoulos@yahoo.com>
2022-02-17 06:15:30 -0500

    The projects page is laid out correctly and to include the jumbotron,rows with random and top 5 projects. I will need to add a tags for clickijng on each image and make minor adjustments

--- client/src/components/ProjectItems/ProjectItems.js
+++ client/src/components/ProjectItems/ProjectItems.js
@@ -3,83 +3,41 @@ import Row from 'react-bootstrap/Row';
 import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
 import './style.css';
-import styled from 'styled-components';
-import Spinner from '../Spinner/Spinner';
+import Spinner from 'react-bootstrap/Spinner';
+
 import PropTypes from 'prop-types';
 import formatDate from '../utils/formatDate';
 
-// import GolfImages from '../images/golf.jpg';
-// import EmployeeImage from '../images/employee-directory.jpg';
-// import FitImage from '../images/fit-image.jpg';
-// import BuildAPageImage from '../images/build-a-page.jpg';
-import DevSquadImage from '../../images/dev-squad.jpg';
-import ExcavationImage from '../../images/excavation.jpg';
-import ContactBookImage from '../../images/contact-book.jpg';
-import GithubFinderImage from '../../images/github-finder.jpg';
-import ITLoggerImage from '../../images/it-logger.jpg';
-// ADD ONS CAROUSEL AND BOX REVEALER
-
-const ProjectItems = ({ loading,isLarge, imageMatch}) => {
-  // // * IMAGES IN ARRAY FOR EACH OF THE TOP 5 POSTERS
-  // // *Ideally will come from the unsplash api and made as a piece of state merged with the github api
-  // const imagesOf5 = [
-  //   {
-  //     id: 0,
-  //     imageId: 435850053,
-  //     imageRep: ITLoggerImage,
-  //     imageName: 'IT-Logger'
-  //   },
-  //   {
-  //     id: 1,
-  //     imageId: 426586061,
-  //     imageRep: ContactBookImage,
-  //     imageName: 'Contact-Book'
-  //   },
-  //   {
-  //     id: 2,
-  //     imageId: 421770170,
-  //     imageRep: GithubFinderImage,
-  //     imageName: 'Github-Finder'
-  //   },
-  //   {
-  //     id: 3,
-  //     imageId: 391642169,
-  //     imageRep: ExcavationImage,
-  //     imageName: 'GP-Excavation'
-  //   },
-  //   {
-  //     id: 4,
-  //     imageId: 383392571,
-  //     imageRep: DevSquadImage,
-  //     imageName: 'DevSquad'
-  //   }
-  // ];
- 
-
-  if (loading) return <Spinner />;
+const ProjectItems = ({ loading, isLarge, projects, top5 }) => {
+  if (loading) return <Spinner animation='border' variant='info' />;
+  const projectsCopy = projects;
+  const { imageCheck } = projectsCopy;
   return (
     <Fragment>
+      {imageCheck.map((item) =>
+        top5 && item.id >= 0 && item.id <= 4 ? (
+          <img
+            className={`${isLarge ? 'row-poster' : 'small-row-poster'}`}
+            key={item.id}
+            src={item.imageRep}
+            title={item.imageName}
+            alt={`Coming soon ${item.imageName}`}
+          />
+        ) : null
+      )}
 
-            {/* {
-              <a
-                href={homepage}
-                target='_blank'
-                rel='noreferrer noopener'
-                alt={`Name of the repo ${name}`}>
-                {name}
-              </a>}   */}
-
-<div className='row-posters'>
-         {imageMatch.map((item) => 
-        <img
-        className={`${isLarge? 'row-poster':'small-row-poster'}`}
-        key={item.id}
-        src={item.imageRep}
-        alt={`Coming soon ${item.imageName}`}
-        /> 
-
-          )}
-          </div>
+      {imageCheck.map(
+        (item) =>
+          top5 === false && (
+            <img
+              className={`${isLarge ? 'row-poster' : 'small-row-poster'}`}
+              key={item.id}
+              src={item.imageRep}
+              title={item.imageName}
+              alt={`Coming soon ${item.imageName}`}
+            />
+          )
+      )}
     </Fragment>
   );
 };
--- client/src/pages/Projects.js
+++ client/src/pages/Projects.js
@@ -3,11 +3,11 @@ import React, { useEffect, useState, Fragment } from 'react';
 import ProjectItems from '../components/ProjectItems/ProjectItems';
 // import Footer from "../components/Footer";
 import Row from 'react-bootstrap/Row';
-// import Container from 'react-bootstrap/Container';
+import Spinner from 'react-bootstrap/Spinner';
 // import Unsplash, { toJson } from "unsplash-js";
 
 // PROJECT IMAGES
-// import GolfImages from '../images/golf.jpg';
+import GolfImages from '../images/golf.jpg';
 // import EmployeeImage from '../images/employee-directory.jpg';
 // import FitImage from '../images/fit-image.jpg';
 // import BuildAPageImage from '../images/build-a-page.jpg';
@@ -37,49 +37,69 @@ const Projects = () => {
   }
 
   //*STATE
-  // GET the github data
-  //loading state
   const [loading, setLoading] = useState(false);
-  const [projects, setProjects] = useState([]);
-  const [top5Repos, setTop5Repos] = useState([]);
+  const [projects, setProjects] = useState({
+    repos: [],
+    imageCheck: []
+  });
   const [error, setError] = useState(false);
 
-  // * IMAGES IN ARRAY FOR EACH OF THE TOP 5 POSTERS
-  // *Ideally will come from the unsplash api and made as a piece of state merged with the github api
+  // * IMAGES IN ARRAY ALL REPOS
+  // *Ideally will come from the unsplash api and made as a piece of state merged with the github api ID numbers--I hardcoded these in
+  // The repos relationships are id and imageId
   const [images, setImages] = useState([
     {
       id: 0,
       imageId: 435850053,
       imageRep: ITLoggerImage,
-      imageName: 'IT-Logger'
+      imageName: 'IT-Logger-App',
+      repo: {}
+      // repo: '' added in by useEffect
     },
     {
       id: 1,
       imageId: 426586061,
       imageRep: ContactBookImage,
-      imageName: 'Contact-Book'
+      imageName: 'Contact-Book',
+      repo: {}
+      // repo: '' added in by useEffect
     },
     {
       id: 2,
       imageId: 421770170,
       imageRep: GithubFinderImage,
-      imageName: 'Github-Finder'
+      imageName: 'Github-Finder',
+      repo: {}
+      // repo: '' added in by useEffect
     },
     {
       id: 3,
       imageId: 391642169,
       imageRep: ExcavationImage,
-      imageName: 'GP-Excavation'
+      imageName: 'GP-Excavation',
+      repo: {}
+
+      // repo: '' added in by useEffect
     },
     {
       id: 4,
       imageId: 383392571,
       imageRep: DevSquadImage,
-      imageName: 'DevSquad'
+      imageName: 'Dev-Squad',
+      repo: {}
+      // repo: '' added in by useEffect
+    },
+    {
+      id: 5,
+      imageId: 365557403,
+      imageRep: GolfImages,
+      imageName: 'Golf-Assistant-App',
+      repo: {}
+      // repo: '' added in by useEffect
     }
   ]);
-
-  console.log('projects', projects);
+  //* DESTRUCTURE IMAGE STATE ARRAY
+  const imageMatch = images;
 
   // *VARIABLES FOR THE GITHUB API
   const baseUrl = `https://api.github.com/users/GregPetropoulos`;
@@ -91,29 +111,43 @@ const Projects = () => {
 
   // console.log('baseUrlUnsplash',baseUrlUnsplash);
 
-  //*THE CALL TO GH *GET THE TOP 5
+  //*THE CALL TO GH *GET ALL REPOS
   useEffect(() => {
     console.log('useEffect project axios call here');
-    // loader while function gets response
-    // calling on the github api
+
     const getAllRepos = async () => {
       try {
         setError(false);
         setLoading(true);
         const res = await axios.get(allRepoUrl);
         const results = res.data;
-        // setProjects(prev=> ({...prev,[results]:results}))
-        setProjects([...results]);
 
-        console.log('res.data', res.data);
-        // console.log('projects.results', projects.results[5]);
-        console.log('projects', projects);
         // return res;
+        //* ADDING IMAGES AND REPOS TO PROJECTS STATE
+        setProjects(() => ({
+          repos: [...results],
+          imageCheck: [...imageMatch]
+        }));
+        console.log('res.data', res.data);
+
+        //* MAPPING THROUGH THE IMAGE STATE ADDING REPO OBJECT TO IMAGES STATE TO THE IMAGE PER IMAGEID
+        const resultsCopy = results;
+        setImages((prev) => {
+          return prev.map((p) => ({
+            ...p,
+            ...{
+              repo: resultsCopy.find(
+                (item) => item.id === p.imageId && item.homepage
+              )
+            }
+          }));
+        });
       } catch (error) {
         setError(true);
       }
       setLoading(false);
     };
+
     getAllRepos();
 
     //* calling on the unsplash api
@@ -127,48 +161,27 @@ const Projects = () => {
     // repoUnsplashImages()
   }, []);
 
-  // *RETURNS TOP 5 ARRAY AND HARDCODED IMAGES ARRAY
+  //* Randomly generating image for the Jumbotron
+  const imageCopy = images;
+  const randomNum = Math.floor(Math.random() * imageCopy.length);
+  const randomJumboImage = imageCopy.filter((item) => item.id === randomNum);
+  console.log('randomJumboImage', randomJumboImage);
 
-  // console.log("projects.results", projects.results[5]);
-
-  // *RETURNS RANDOM
-  const isRandom = () => {
-    if (projects !== null) {
-      const randMatch = () =>
-        projects.filter((item) => !images.find((t5) => item.id === t5.imageId));
-      if (randMatch) {
-        Math.floor(Math.random() * randMatch.length);
-        return randMatch();
-      }
-    }
-  };
+  //* Truncate ellipse function from stack overflow
+  function truncate(str, n) {
+    return str?.length > n ? str.substr(0, n - 1) + '...' : str;
+  }
 
   // * NO TRENDING FUNCTION NEEDED FOR TRENDING CATEGORY SINCE GITHUB API IS RETURNING THE LATEST REPOS
   // console.table(projects)
   // console.log('KEY CHECK HERE',process.env.MY_ACCESS);
   // console.log('KEY CHECK HERE',process.env.REACT_APP_MY_FAKE_KEY);
-  // const {imageRep}= top5Repos
 
-  const top5Arr = projects.filter((item) =>
-    images.find((t5) => item.id === t5.imageId)
-  );
-  const imageMatch = images;
-  const combo5And5 = top5Arr.concat(imageMatch);
-  const adder = () => {
-    setTop5Repos(combo5And5);
-  };
-
-  // const comboRepImg = () => {
-  // top5Arr.concat(imageMatch)
-  // setTop5Repos([top5Arr, images])
-  // }
-  // const comboRepImg = projects.filter((item) => top5.find((t5) => item.id === t5.id).map(arr5=> imageMatch.filter(img=> imageMatch.imageId===arr5.id) ))
-  // console.log("comboRepImg", comboRepImg);
-  // console.log('comboRepImg',comboRepImg());
-  console.log('top5Repos', top5Repos);
-  console.log('imagematch', imageMatch);
-  console.log('top5Arr', top5Arr);
-  console.log(loading === true);
+  console.log('images', images);
+  console.log('projects', projects);
+  // To get home page link use below:
+  // console.log('imageA -- check', images[3].repo.homepage)
+  if (loading) return <Spinner animation='border' variant='info' />;
   return (
     <Fragment>
       <Row>
@@ -181,22 +194,72 @@ const Projects = () => {
           &lt;projects&gt;
         </p>
       </Row>
+      <div>
+        <header
+          className='jumbo'
+          style={{
+            backgroundSize: 'cover',
 
+            backgroundImage: `url(${randomJumboImage[0].imageRep})`,
+            backgroundPosition: 'center center'
+          }}>
+          <div className='jumbo-contents'>
+            <h1 className='jumbo-title'>{randomJumboImage[0].imageName}</h1>
+            <div className='jumbo-buttons'>
+              <button className='jumbo-button'>Play</button>
+              <button className='jumbo-button'>My List</button>
+            </div>
+            <h1 className='jumbo-description'>
+              {truncate(randomJumboImage[0].repo.description, 150)}
+            </h1>
+          </div>
+          <div className='jumbo-fade-bottom' />
+        </header>
+      </div>
       <div className='top-5-row'>
         <h2>My Top 5 Repos</h2>
-
-        <ProjectItems isLarge={true} imageMatch={imageMatch} loading={loading} />
+        <div className='row-posters'>
+          <ProjectItems
+            loading={loading}
+            projects={projects}
+            isLarge={true}
+            top5={true}
+          />
+        </div>
       </div>
       <div className='trending-row'>
         <h2>Trending</h2>
-
-        <ProjectItems isLarge={false} imageMatch={imageMatch} loading={loading} />
+        <div className='row-posters'>
+          <ProjectItems
+            isLarge={false}
+            projects={projects}
+            loading={loading}
+            top5={false}
+          />
+        </div>
       </div>
-      <div className='random'>
+      <div className='random-row'>
         <h2>Random</h2>
-
-        <ProjectItems isLarge={false} imageMatch={imageMatch} loading={loading} />
+        <div className='row-posters'>
+          <ProjectItems
+            isLarge={false}
+            projects={projects}
+            loading={loading}
+            top5={false}
+          />
+        </div>
       </div>
+
+      <Row>
+        <p
+          style={{
+            fontFamily: 'monospace',
+            fontSize: '1.4rem',
+            color: '#d6b850'
+          }}>
+          &lt;/projects&gt;
+        </p>
+      </Row>
     </Fragment>
   );
 };
