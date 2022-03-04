06758fba967b1e486a46d8f49dc9ca9de3cc7e10
GP <gregpetropoulos@yahoo.com>
2022-03-04 17:14:14 -0500

    Changed image sizes and removed typewriter-effect.

--- client/src/components/landing/Land.js
+++ client/src/components/landing/Land.js
@@ -1,44 +1,16 @@
 import React, { Fragment } from 'react';
-import Typewriter from 'typewriter-effect';
 import MainFooter from '../mainFooter/MainFooter';
 
 const Land = () => {
   return (
     <Fragment>
-      {/* <Container> */}
-      {/* <Row>
-        <Col lg={8}> */}
-        
-        
-   <div className="land-wrapper">
-
-
-          <h2>
-            <Typewriter
-              onInit={(typewriter) => {
-                typewriter
-                  .pauseFor(1500)
-                  .changeDelay(80)
-                  .typeString(
-                    'Hi, happy to see you made it over. My name is Greg Petropoulos and I enjoy creating websites and web applications.'
-                  )
-                  .pauseFor(2500)
-                  .deleteAll(10)
-                  .typeString('Hang out and get to know me.')
-                  .pauseFor(3000)
-                  .deleteAll(20)
-                  .typeString('Greg Petropoulos')
-                  .pauseFor(15000)
-                  .deleteAll(20)
-                  .start();
-              }}
-            />
-          </h2>
-          {/* </Col>
-      </Row> */}
-      {/* </Container> */}
+      <div className='land-wrapper'>
+        <h1>Greg Petropoulos</h1>
+        <br></br>
+        <h3>Hi :)</h3>
       </div>
-<MainFooter/>
+
+      <MainFooter />
     </Fragment>
   );
 };
