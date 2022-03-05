3f5d7f42423b9a09eb94ed40b3af5e64e6cfae0f
GP <gregpetropoulos@yahoo.com>
2022-03-05 12:28:48 -0500

    tshooting

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,28 +1,30 @@
 import React from 'react';
+
 import AboutSection from '../components/about/AboutSection';
-import Row from 'react-bootstrap/Row';
 import Container from 'react-bootstrap/Container';
 // import Col from "react-bootstrap/Col";
 
 const About = () => {
   return (
     <Container>
-      
-        <p className='page-title'
-          style={{
-            fontSize: '1.4rem',
-            color: '#08a7ce', 
-          }}>
-          &lt;about&gt;
-        </p>
+      <p
+        className='page-title'
+        style={{
+          fontSize: '1.4rem',
+          color: '#08a7ce'
+        }}>
+        &lt;about&gt;
+      </p>
+
       <AboutSection />
-        <p className='page-title'
-          style={{
-            fontSize: '1.4rem',
-            color: '#08a7ce'
-          }}>
-          &lt;/about&gt;
-        </p>
+      <p
+        className='page-title'
+        style={{
+          fontSize: '1.4rem',
+          color: '#08a7ce'
+        }}>
+        &lt;/about&gt;
+      </p>
     </Container>
   );
 };
