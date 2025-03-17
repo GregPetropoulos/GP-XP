b7731d19d413bf4ca671ba450c96a641378b2dd2
GP <gregpetropoulos@yahoo.com>
2025-03-17 00:14:32 -0400

    Removed old portfolio for vite prep

--- src/utils/index.js
+++
@@ -1,9 +0,0 @@
-const formatDate = (date) => {
-  return new Intl.DateTimeFormat().format(new Date(date));
-};
-
-const truncateString = (str, n) => {
-  return str?.length > n ? str.substr(0, n - 1) + '...' : str;
-};
-
-export { truncateString, formatDate };
