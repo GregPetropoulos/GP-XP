dc6d6d7d43022d119fb90c97a4ae5e074443eb39
GP <gregpetropoulos@yahoo.com>
2025-03-29 20:56:07 -0400

    Setting up global page title over the <Outlet/>

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,16 +1,31 @@
-import { Outlet, createRootRoute} from '@tanstack/react-router';
+import { Outlet, createRootRoute } from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
+import { useLocation } from '@tanstack/react-router';
 import NavBar from '../components/NavBar';
 import Footer from '../components/Footer';
-
+import ContentContainer from '../components/ContentContainer';
+import PageTitle from '../components/PageTitle';
+import { NAV_LINKS } from '../constants/constants';
+import type { NavLinks } from '../models';
 
 export const Route = createRootRoute({
   component: () => (
     <>
       <NavBar />
-      <Outlet />
+      <ContentContainer>
+        {NAV_LINKS.map((item: NavLinks) => {
+          const location = useLocation();
+          if (location.pathname === item.to) {
+            return <PageTitle title={item.label} />;
+          } else {
+            return null;
+          }
+        })}
+
+        <Outlet />
+      </ContentContainer>
       <TanStackRouterDevtools />
-      <Footer/>
+      <Footer />
     </>
   )
 });
