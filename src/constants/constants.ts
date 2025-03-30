fb31945130a0f3dea09746336e8116255d4706e9
GP <gregpetropoulos@yahoo.com>
2025-03-29 20:03:09 -0400

    Add interface to nav links, change nav links data structure, pretty up active nav links in mobile and desktop views

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,9 +1,14 @@
-export const NAV_LINKS: string[] = [
-  'Home',
-  'About',
-  'Projects',
-  'Blog',
-  'Contact'
+
+
+import  { type NavLinks } from "../models";
+
+
+export const NAV_LINKS:NavLinks[]= [
+  {to:'/',label:'Home'},
+  {to:'/about',label:'About'},
+  {to:'/projects',label:'Projects'},
+  {to:'/blog',label:'Blog'},
+  {to:'/contact',label:'Contact'}
 ];
 export const CORE_SKILLS: string[] = [
   'JavaScript',
