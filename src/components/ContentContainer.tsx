cdc986ee6d62e39eed992abc00cad12a43272471
GP <gregpetropoulos@yahoo.com>
2025-03-30 23:19:24 -0400

    Check off content container

---
+++ src/components/ContentContainer.tsx
@@ -0,0 +1,11 @@
+
+
+const ContentContainer = ({ children }: any) => {
+  return (
+    <div className='flex w-full justify-center px-2.5 sm:px-5 flex-wrap'>
+      {children}
+    </div>
+  );
+};
+
+export default ContentContainer;
