c1658af431f2526204db0dbcf6bb2531c5c2e722
GP <gregpetropoulos@yahoo.com>
2025-03-30 23:18:12 -0400

    Page title rework

--- src/components/PageTitle.tsx
+++ src/components/PageTitle.tsx
@@ -1,9 +1,11 @@
-
-const PageTitle = ({title}:{title:string}) => {
-
+const PageTitle = ({ title }: { title: string }) => {
   return (
-    <h2 className='text-center text-3xl sm:text-4xl my-4'>{title}</h2>
-  )
-}
+    <div className='flex  justify-center w-full lg:hidden'>
+      <h2 className='text-center w-full text-3xl sm:text-4xl my-4  '>
+        {title}
+      </h2>
+    </div>
+  );
+};
 
-export default PageTitle
+export default PageTitle;
