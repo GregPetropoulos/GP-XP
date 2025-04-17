60eebe7ef54b092c9f4a6dc2718f02cb8c3d6be0
GP <gregpetropoulos@yahoo.com>
2025-04-17 00:51:32 -0400

    Mobile styles for page title and theme added in

--- src/components/PageTitle.tsx
+++ src/components/PageTitle.tsx
@@ -1,6 +1,6 @@
 const PageTitle = ({ title }: { title: string }) => {
   return (
-      <h1 className=' w-full my-4'>
+      <h1 className=' w-full text-center my-4'>
         {title}
       </h1>
    
